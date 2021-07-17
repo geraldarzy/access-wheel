ee5801e6a170e6cf2dc706b553370fb6a9562c7a
harsh <harshvardhan.modernschool@gmail.com>
2021-07-18 04:39:58 +0530

    form by harsh

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,42 @@
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap"
-import logo from '../pictures/access-wheel-logo.png'
-import newlogo from '../pictures/newlogo.png'
-import './NavBar.css'
-const NavBar = () => {
-    return (
-        <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home" ><img id='landingpageLogo' src={newlogo}/></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar> 
-        </>
-    )
-}
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../pictures/access-wheel-logo.png";
+import newlogo from "../pictures/newlogo.png";
+import "./NavBar.css";
+import { Link, NavLink } from "react-router-dom";
 
-export default NavBar
+const NavBar = () => {
+	return (
+		<>
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">
+						<img id="landingpageLogo" src={newlogo} />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<Nav.Link href="#newloc">Add New Location</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">
+									Another action
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">
+									Something
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">
+									Separated link
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
+
+export default NavBar;
